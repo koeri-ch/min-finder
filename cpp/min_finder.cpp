f0c155c853d0f516e8a0a98e4876705871efd804
Luan Koerich <koerich@localhost-live.gateway.sasktel.net>
2020-09-23 13:58:29 -0600

    Adding the C++ function to the repository.

---
+++ cpp/min_finder.cpp
@@ -0,0 +1,50 @@
+void min_finder(std::vector<float> hwaveform, float threshold) {
+
+    // extrema will contain the indices of 
+    // the waveform vector that contain a minimum.
+    std::vector<int> extrema;
+
+    // N is the number of samples in
+    // the waveform vector.
+    int N = hwaveform.size();
+
+    // We start at one sample after the 
+    // beginning of the waveform.
+    int i = 1; 
+    while (i < N) {
+
+        // We are going to compare the current sample i
+        // to the ones ahead and before.
+        int i_ahead = i + 1;
+        int i_behind = i - 1;
+
+        // Compare the current sample to the threshold
+        // before determining the minima. This saves time.
+        if (hwaveform[i] < threshold) {
+
+            // Compare the current sample to all the following
+            // and keep going, unless the ahead sample is different
+            // than the current sample.
+            while (hwaveform[i_ahead] == hwaveform[i] && i_ahead < N-1) {
+                i_ahead++;
+            }
+
+            // Compare the sample ahead to the one before 
+            // to find out if the current sample is a minimum.
+            if (hwaveform[i_ahead]>hwaveform[i] && hwaveform[i_behind]>hwaveform[i] ) {
+    
+                // Find midpoint between ahead and before, 
+                // or close to it. This will useful if the 
+                // minimun is a plateau.
+                int k = floor((i_ahead + i_behind)/2);
+                extrema.push_back(k);
+            }
+        }
+        
+        // The show must go on.
+        i++;
+    } 
+
+    // Return the vector to the caller.
+    return extrema;       
+}
